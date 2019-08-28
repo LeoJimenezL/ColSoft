79b4893f68a9aa03c43a30220b5adde397455001
Estudiante <Estudiante@S106PC11>
2019-08-28 09:21:08 -0500

    Se creo el modelo vista controlador (MVC)

--- src/modelo/NivelFormacion.java
+++ src/modelo/NivelFormacion.java
@@ -1,6 +1,33 @@
  
 package modelo;
 
-public class NivelFormacion {
-    
+public class NivelFormacion 
+{
+    private int idNivelFormacion;
+    private String nombre;
+    private String estado;
+
+    public int getIdNivelFormacion() {
+        return idNivelFormacion;
+    }
+
+    public void setIdNivelFormacion(int idNivelFormacion) {
+        this.idNivelFormacion = idNivelFormacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
